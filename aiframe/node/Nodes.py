06e1578f00d5985686f03e792592526f501ba4ef
CodingTrue <codingtrue@web.de>
2025-05-13 16:55:05 +0200

    Added basic nodes

--- aiframe/node/Nodes.py
+++ aiframe/node/Nodes.py
@@ -14,3 +14,21 @@ class BaseActivationNode(BaseNode):
     @abstractmethod
     def backward(self):
         return
+
+class HiddenLayerNode(BaseNode):
+    def __init__(self, neuron_count: int = 0, input_size: int = 0):
+        self._neuron_count = neuron_count
+        self._input_size = input_size
+
+        self._weights = None
+        self._biases = None
+
+    def evaluate(self):
+        self._output = np.dot(self._input, self._weights.T) + self._biases
+
+class ReluActivation(BaseActivationNode):
+    def evaluate(self):
+        self._output = np.max(self._input)
+
+    def backward(self):
+        return self._output > 0
