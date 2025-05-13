904219afcd9ee21485899e7c26410e3b33717a36
CodingTrue <codingtrue@web.de>
2025-05-13 17:06:46 +0200

    Added automatic input allocation

--- aiframe/NeuralNetwork.py
+++ aiframe/NeuralNetwork.py
@@ -1,7 +1,23 @@
 from aiframe.node import NodeLoader, BASIC_NODE_LOADER
 from aiframe.node.Nodes import *
 
+import numpy as np
+
 class NeuralNetwork():
     def __init__(self, network_nodes: list[BaseNode] = [], max_input_size: int = 0, nodeloader: NodeLoader = BASIC_NODE_LOADER):
         self._network_nodes = network_nodes
+        self._max_input_size = max_input_size
         self._nodeloader = nodeloader
+
+    def allocate_input_dimensions(self, random: bool = True):
+        input_size = self._max_input_size
+
+        for node in self._network_nodes:
+            if not self._nodeloader.is_layer(target=node): continue
+
+            node._input_size = input_size
+            input_size = node._neuron_count
+
+            node._weights = np.random.uniform(-1, 1, (node._neuron_count, node._input_size)) if random else np.zeros((node._neuron_count, node._input_size))
+            node._biases = np.random.uniform(-1, 1, (node._neuron_count,)) if random else np.zeros((node._neuron_count,))
+        return self
