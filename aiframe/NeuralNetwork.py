79bd9573982496abcf9142a1099bc57b123c9908
CodingTrue <codingtrue@web.de>
2025-05-26 14:24:34 +0200

    Added logic to apply gradients

--- aiframe/NeuralNetwork.py
+++ aiframe/NeuralNetwork.py
@@ -12,9 +12,7 @@ class NeuralNetwork():
     def allocate_input_dimensions(self, random: bool = True):
         input_size = self._max_input_size
 
-        for node in self._network_nodes:
-            if not self._nodeloader.is_layer(target=node): continue
-
+        for node in self.get_layers():
             node._input_size = input_size
             input_size = node._neuron_count
 
@@ -22,8 +20,20 @@ class NeuralNetwork():
             node._biases = np.random.uniform(-1, 1, (node._neuron_count,)) if random else np.zeros((node._neuron_count,))
         return self
 
+    def get_layer_at_index(self, index: int) -> BaseNode:
+        return self.get_layers()[index]
+
+    def get_weights(self) -> np.ndarray:
+        return [node._weights for node in self.get_layers()]
+
+    def get_biases(self) -> np.ndarray:
+        return [node._biases for node in self.get_layers()]
+
     def get_network_structure(self) -> list[(int, int)]:
         return [(layer._input_size, layer._neuron_count) for layer in self._network_nodes if self._nodeloader.is_layer(target=layer)]
 
+    def get_layers(self) -> np.ndarray:
+        return [node for node in self._network_nodes if self._nodeloader.is_layer(target=node)]
+
     def get_layer_count(self) -> int:
-        return len([node for node in self._network_nodes if self._nodeloader.is_layer(target=node)])
+        return len(self.get_layers())
--- aiframe/program/TrainProgram.py
+++ aiframe/program/TrainProgram.py
@@ -4,13 +4,24 @@ from aiframe import NeuralNetwork
 from aiframe.program import Program
 
 class TrainProgram(Program):
-    def train(self, nn: NeuralNetwork, training_data: list, learn_rate: float = 0.05):
+    def train(self, nn: NeuralNetwork, training_data: list, learn_rate: float = 0.05, iterations: int = 1):
         train_function = self.get_function(function_name="train_function", globals={"np": np})
+
+        weights = nn.get_weights()
+        biases = nn.get_biases()
+        layer_count = nn.get_layer_count()
 
         gradientW = [np.zeros(layer) for layer in nn.get_network_structure()]
         gradientB = [np.zeros(neuron_count) for _, neuron_count in nn.get_network_structure()]
 
-        inputs = np.array([[1, 2, 1], [2, 2, 1]])
-        expected = np.array([2, 2])
+        for i in range(iterations):
+            train_function(inputs=training_data[0], expected=training_data[1], gradientW=gradientW, gradientB=gradientB)
 
-        train_function(inputs=inputs, expected=expected, gradientW=gradientW, gradientB=gradientB)
+            for i in range(layer_count):
+                weights[i] -= gradientW[i] * learn_rate
+                biases[i] -= biases[i] * learn_rate
+
+        for i in range(layer_count):
+            layer = nn.get_layer_at_index(index=i)
+            layer._weights = weights[i]
+            layer._biases = biases[i]
